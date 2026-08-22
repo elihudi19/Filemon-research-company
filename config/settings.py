784524197ded9cf6434b713525d2803c43e00462
elihudi <elihudi.eliamini@gmail.com>
2026-08-22 20:39:23 +0300

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -63,6 +63,7 @@ MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "core.middleware.SiteLanguageMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
