26368957380cf3022910b3e162b1ac9af17fba88
elihudi <elihudi.eliamini@gmail.com>
2026-08-27 08:04:09 +0300

    Add company tagline based on language setting

--- core/context_processors.py
+++ core/context_processors.py
@@ -2,8 +2,11 @@ from django.conf import settings
 
 
 def company_context(request):
+    lang = getattr(request, "LANG", "en")
+    tagline = settings.COMPANY_FULL_NAME_EN if lang == "en" else settings.COMPANY_FULL_NAME_SW
     return {
         "COMPANY_NAME": settings.COMPANY_NAME,
         "COMPANY_SHORT_NAME": settings.COMPANY_SHORT_NAME,
-        "LANG": getattr(request, "LANG", "en"),
+        "COMPANY_TAGLINE": tagline,
+        "LANG": lang,
     }
