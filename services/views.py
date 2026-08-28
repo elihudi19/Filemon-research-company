b796ccd072ef3b1a8b1d5f5147cd308f26bc4362
elihudi <elihudi.eliamini@gmail.com>
2026-08-28 13:23:49 +0300

    Add service detail view with slug filtering

--- services/views.py
+++ services/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 from .models import Service
 
@@ -6,3 +6,8 @@ from .models import Service
 def service_list(request):
     services = Service.objects.filter(is_published=True)
     return render(request, "services/list.html", {"services": services})
+
+
+def service_detail(request, slug):
+    service = get_object_or_404(Service, slug=slug, is_published=True)
+    return render(request, "services/detail.html", {"service": service})
