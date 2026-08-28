205faaa78ad15bee9141e43bc6c943cc5e0761b1
elihudi <elihudi.eliamini@gmail.com>
2026-08-28 13:25:12 +0300

    Add prepopulated_fields to ServiceAdmin

--- services/admin.py
+++ services/admin.py
@@ -7,4 +7,5 @@ from .models import Service
 class ServiceAdmin(admin.ModelAdmin):
     list_display = ("title_en", "is_published", "order")
     list_filter = ("is_published",)
+    prepopulated_fields = {"slug": ("title_en",)}
     ordering = ("order",)
