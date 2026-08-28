661186bfbb1c5a68206d0a4f38728555b8cbca37
elihudi <elihudi.eliamini@gmail.com>
2026-08-28 13:22:07 +0300

    Add slug field and get_absolute_url method to Service

--- services/models.py
+++ services/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.urls import reverse
 
 
 class Service(models.Model):
     title_en = models.CharField(max_length=150)
     title_sw = models.CharField(max_length=150)
+    slug = models.SlugField(unique=True, blank=True)
     description_en = models.TextField()
     description_sw = models.TextField()
     icon = models.CharField(max_length=50, default="clipboard")
@@ -15,3 +17,6 @@ class Service(models.Model):
 
     def __str__(self):
         return self.title_en
+
+    def get_absolute_url(self):
+        return reverse("services:detail", kwargs={"slug": self.slug})
